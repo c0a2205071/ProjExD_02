4b152cd70f3f308e59ee0641cf96329dad1a6d14
atsukan <c0a2205071@edu.teu.ac.jp>
2023-04-25 14:51:18 +0900

    練習問題４

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,15 @@
-import random  #練習2
+import random  # 練習2
 import sys
 
 import pygame as pg
+
+delta = {
+         pg.K_UP:(0, -1) ,
+         pg.K_DOWN: (0, 1),
+         pg.K_LEFT: (-1, 0),
+         pg.K_RIGHT: (1, 0),
+         }
+ # 練習4
 
 
 def main():
@@ -13,24 +21,31 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
 
     tmr = 0
-    bb_image = pg.Surface((20, 20))  #練習1
-    pg.draw.circle(bb_image, (255, 0, 0), (10, 10), 10)  #練習1
-    bb_image.set_colorkey((0, 0, 0))  #練習1
-    x, y = random.randint(0, 1600), random.randint(0, 900)  #練習2
-    screen.blit(bb_image, [x, y])  #練習2
-    vx, vy = +1, +1  #練習3
-    bb_rect = bb_image.get_rect()  #練習3
-    bb_rect.center = (x, y)  #練習3
+    bb_image = pg.Surface((20, 20))  # 練習1
+    pg.draw.circle(bb_image, (255, 0, 0), (10, 10), 10)  # 練習1
+    bb_image.set_colorkey((0, 0, 0))  # 練習1
+    x, y = random.randint(0, 1600), random.randint(0, 900)  # 練習2
+    screen.blit(bb_image, [x, y])  # 練習2
+    vx, vy = +1, +1  # 練習3
+    bb_rect = bb_image.get_rect()  # 練習3
+    bb_rect.center = (x, y)  # 練習3
+    kk_rect = kk_img.get_rect()  # 練習4
+    kk_rect.center = (900, 400)   # 練習4
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return 0
         tmr += 1
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        bb_rect.move_ip(vx, vy)  #練習3
-        screen.blit(bb_image, bb_rect)
+
+        key_list = pg.key.get_pressed()  # 練習4
+        for k, mv in delta.items():  # 練習4
+            if key_list[k]:  # 練習4
+                kk_rect.move_ip(mv)  # 練習4
+        screen.blit(bg_img, [0, 0])  # 練習4
+        bb_rect.move_ip(vx, vy)  # 練習3
+        screen.blit(bb_image, bb_rect)  # 練習3
+        screen.blit(kk_img, kk_rect)  # 練習4
 
         pg.display.update()
         clock.tick(1000)
